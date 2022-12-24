004bfd87e39244fac28ee14a2a13443b80926c79
Tualin14 <t@dzaaaaaa.com>
2022-12-24 22:52:50 +0800

    fix WebGL Input

---
+++ Assets/WebGLSupport/WebGLWindow/WebGLWindow.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using AOT;
+using System.Runtime.InteropServices; // for DllImport
+using UnityEngine;
+
+namespace WebGLSupport
+{
+    static class WebGLWindowPlugin
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        public static extern void WebGLWindowInit();
+        [DllImport("__Internal")]
+        public static extern void WebGLWindowOnFocus(Action cb);
+
+        [DllImport("__Internal")]
+        public static extern void WebGLWindowOnBlur(Action cb);
+
+        [DllImport("__Internal")]
+        public static extern void WebGLWindowOnResize(Action cb);
+
+        [DllImport("__Internal")]
+        public static extern void WebGLWindowInjectFullscreen();
+#else
+        public static void WebGLWindowInit() { }
+        public static void WebGLWindowOnFocus(Action cb) { }
+        public static void WebGLWindowOnBlur(Action cb) { }
+        public static void WebGLWindowOnResize(Action cb) { }
+        public static void WebGLWindowInjectFullscreen() { }
+#endif
+
+    }
+
+    public static class WebGLWindow
+    {
+        static WebGLWindow()
+        {
+            WebGLWindowPlugin.WebGLWindowInit();
+        }
+        public static bool Focus { get; private set; }
+        public static event Action OnFocusEvent = () => { };
+        public static event Action OnBlurEvent = () => { };
+        public static event Action OnResizeEvent = () => { };
+
+        static string ViewportContent;
+        static void Init()
+        {
+            Focus = true;
+            WebGLWindowPlugin.WebGLWindowOnFocus(OnWindowFocus);
+            WebGLWindowPlugin.WebGLWindowOnBlur(OnWindowBlur);
+            WebGLWindowPlugin.WebGLWindowOnResize(OnWindowResize);
+            WebGLWindowPlugin.WebGLWindowInjectFullscreen();
+        }
+
+        [MonoPInvokeCallback(typeof(Action))]
+        static void OnWindowFocus()
+        {
+            Focus = true;
+            OnFocusEvent();
+        }
+
+        [MonoPInvokeCallback(typeof(Action))]
+        static void OnWindowBlur()
+        {
+            Focus = false;
+            OnBlurEvent();
+        }
+
+        [MonoPInvokeCallback(typeof(Action))]
+        static void OnWindowResize()
+        {
+            OnResizeEvent();
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void RuntimeInitializeOnLoadMethod()
+        {
+            Init();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 适用于碧蓝档案的二创工具(点击蓝色超链接下载
 
-- 播放器PC [baPlayer](https://github.com/Tualin14/baPlayer/releases)
+- 播放器 PC [baPlayer](https://github.com/Tualin14/baPlayer/releases)
 
-- 素材 
+- 素材
+
   - [Data](https://oss.dza.vin/share@dzaaaaaa.com/baPlayer%E5%8F%AF%E7%94%A8%E7%B4%A0%E6%9D%90%E9%9B%86)
   - 解压到对应文件夹
   - 素材收集于网络，如没有找到自己需要的素材请自行添加。
 
-- [WebGl版](https://github.com/Tualin14/baPlayer_WebGL)
-  - WebGL 目前不在主计划路线中，仅演示播放效果
-  - 在线预览播放Demo文件 [Demo 1.1.1](https://baplayer.dza.vin/)
+- [WebGl 版](https://baplayer.dza.vin/)
+
+  - 在线版音乐不能使用 ogg，应使用 mp3 代替
+  - 为节约空间，不提供 se 和 cover 的在线素材
+  - Data Url 可用来设置素材外链，以便使用自己的素材
+  - 素材在线仅为临时计划，后续有删除可能
 
 # [如何使用](https://github.com/Tualin14/baPlayer/wiki)
 
@@ -20,7 +24,6 @@
 - 在 [Issues](https://github.com/Tualin14/baPlayer/issues) 提交反馈和意见
 - 加入新效果，表情等内容请 **提供游戏内出现位置**
 - 无法播放等问题请提供 **相关文本**
-
 
 # 其它
 
@@ -32,6 +35,7 @@
 - [spine-unity](http://zh.esotericsoftware.com/spine-unity)
 - [UnitySkipSplash](https://github.com/psygames/UnitySkipSplash)
 - [Unity3dRichTextHelper](https://github.com/majecty/Unity3dRichTextHelper)
+- [WebGLInput](https://github.com/kou-yeung/WebGLInput)
 
 # Licence
 
